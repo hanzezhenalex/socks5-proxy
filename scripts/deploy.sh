9b13049731538dcec6861cb5d9341941758606a0
I548435 <alex.han@sap.com>
2023-08-23 13:44:24 +0800

    feat: add test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,4 +4,6 @@ set -euxo pipefail
 
 make docker_server
 
-docker run -d --rm --network host alex/socks5-server:latest --local
+export SEVER_DOCKER_ID
+SEVER_DOCKER_ID=$(docker run -d --rm --network host alex/socks5-server:latest --local)
+echo "${SEVER_DOCKER_ID}"
